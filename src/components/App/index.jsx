3a344a87b8a8deccd9be8c7d3677bb371eb4fe1c
Sylvie Melin <sy.melin@gmail.com>
2023-01-25 16:25:42 +0100

    CreatePost component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react'
-import Header from '../Header'
+import React, { useState, useEffect } from 'react'
 import Login from '../Login'
+import Header from '../Header'
+import CreatePost from '../CreatePost'
 import './App.css'
 
 function App() {
   const [user, setUser] = useState('')
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
     document.title = user ? `${user}'s Feed` : 'Please login';
@@ -20,6 +22,26 @@ function App() {
         user={user}
         setUser={setUser}
       />
+      <CreatePost
+        user={user}
+        posts={posts}
+        setPosts={setPosts}
+      />
+      {posts.map((post, index) => (
+        <React.Fragment
+          key={`post-${user}--${index}`}
+        >
+          {post.image && (
+              <img
+                style={{ width: 200, height: 100, objectFit:"cover"}}
+                src={URL.createObjectURL(post.image)}
+                alt="Post cover"
+              />
+          )}
+          <p>{post.content}</p>
+          <div>{user}</div>
+        </React.Fragment>
+      ))}
     </div>
   )
 }
--- src/components/CreatePost/index.jsx
+++ src/components/CreatePost/index.jsx
@@ -1,9 +1,51 @@
+import { useState, useRef } from 'react';
 import './CreatePost.css';
 
-function CreatePost() {
+function CreatePost({ user, posts, setPosts }) {
+  const [content, setContent] = useState("")
+  const [image, setImage] = useState(null)
+  const imageInputRef = useRef()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const post = {
+      content,
+      image,
+      user,
+    }
+    const newPosts = [post, ...posts]
+    setPosts(newPosts)
+    setContent("")
+    imageInputRef.current.value = ""
+  }
+
   return (
     <div className="createPost">
-      createPost
+      <h1 className="createPost__title">Create New Form</h1>
+      <form
+        className="createPost__form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="createPost__form-input"
+          type="text"
+          placeholder="Add Post Content"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+        />
+        <input
+          className="createPost__form-input"
+          type="file"
+          onChange={(e) => setImage(e.target.files[0])}
+          ref={imageInputRef}
+        />
+        <button
+          className="createPost__button--submit"
+          type="submit"
+        >
+          Submit Post
+        </button>
+      </form>
     </div>
   );
 }
